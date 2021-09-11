489b79d1d25be5f6883acc82147d3813977ca451
mdos-san <mdossan@protonmail.com>
2021-09-12 01:32:08 +0200

    ♻️ Remove boilerplate code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders a title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const mainTitle = screen.getByText(/MuzBox/);
+  expect(mainTitle).toBeInTheDocument();
 });
