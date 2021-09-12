b7ec5e593d7fa7a9faad1d3a345b8f62dc8d87db
mdos-san <mdossan@protonmail.com>
2021-09-12 03:44:58 +0200

    🐛 Fix url in AuthService

--- src/AuthService.js
+++ src/AuthService.js
@@ -4,7 +4,7 @@ const AuthService = () => {
   let decodedJWT = null;
 
   const init = async () => {
-    const response = await fetch("http://localhost:4242/anonymous");
+    const response = await fetch(`${process.env.REACT_APP_AUTH_URL}/anonymous`);
     const json = await response.json();
     decodedJWT = jwt.decode(json.token);
   };
