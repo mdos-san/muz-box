9ed75caca1aee6d89decde78ac19b565935dfc07
mdos-san <mdossan@protonmail.com>
2021-09-12 04:04:04 +0200

    ✨ Use runtime env variable

--- src/AuthService.js
+++ src/AuthService.js
@@ -1,10 +1,12 @@
 import jwt from "jsonwebtoken";
+import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 const AuthService = () => {
+  const env = runtimeEnv();
   let decodedJWT = null;
 
   const init = async () => {
-    const response = await fetch(`${process.env.REACT_APP_AUTH_URL}/anonymous`);
+    const response = await fetch(`${env.REACT_APP_AUTH_URL}/anonymous`);
     const json = await response.json();
     decodedJWT = jwt.decode(json.token);
   };
