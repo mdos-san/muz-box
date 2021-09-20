033a92a947ed927f4330ce737f79e4fdbf653ab9
mdos-san <mdossan@protonmail.com>
2021-09-20 22:33:41 +0200

    ♻️ Use env to define socket-cache url

--- src/SocketService.js
+++ src/SocketService.js
@@ -1,12 +1,14 @@
 import { io } from "socket.io-client";
+import runtimeEnv from '@mars/heroku-js-runtime-env';
 import Watcher from "@mdos-san/watcher";
 
 const SocketService = () => {
   const [watchSocket, setSocket, getSocket] = Watcher(null);
   const [watchCache, setCache, getCache] = Watcher([]);
+  const env = runtimeEnv();
 
   const init = async (token, jwt) => {
-    let socket = io("http://localhost:4241", {
+    let socket = io(env.REACT_APP_SOCKET_CACHE_URL, {
       forceNew: true,
       query: `token=${token}`,
       extraHeaders: { Authorization: `Bearer ${token}` },
