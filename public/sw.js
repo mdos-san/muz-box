19904258d4d4251447bcf95126ed8545a853bb69
mdos-san <mdossan@protonmail.com>
2022-04-03 11:12:07 +0200

    feat: Update icons

--- public/sw.js
+++ public/sw.js
@@ -3,16 +3,11 @@ const VERSION = "V1";
 self.addEventListener( "install", event => {
     self.skipWaiting(); // Activate SW imediatly
 
-    event.waitUntil((async () => {
-       const cache = await caches.open(VERSION);
-        cache.add(new Request('/offline.html'));
-
-    })());
     console.log( "WORKER: install event in progress." );
 });
 
 self.addEventListener( "activate", event => {
-    client.claim(); // Start handling the current page without refreshing
+    clients.claim(); // Start handling the current page without refreshing
     console.log('WORKER: activate event in progress.');
 });
 
